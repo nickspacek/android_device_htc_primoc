acba5477e9e7dbce1ad643503b83752dc4d1679d
Jmz <jmztaylor@gmail.com>
2012-07-27 08:07:31 -0400

    fixing wifi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,9 +60,9 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE                := bcmdhd
-WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_STA          := "/etc/firmware/fw_bcmdhd_b2.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/etc/firmware/fw_bcmdhd_apsta_b2.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/etc/firmware/fw_bcmdhd_p2p_b2.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 BOARD_WLAN_DEVICE_REV            := bcm4330_b1
 
